766bc47537f0bd5f4eec99feeaae16747e9e7d06
Matthias Plancke <matthiasplancke@MacBook-Air-van-Matthias.local>
2019-11-30 17:13:39 +0000

    initial xcode project

---
+++ dev-profile/dev-profile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dev-profile
+//
+//  Created by Matthias Plancke on 30/11/2019.
+//  Copyright © 2019 Matthias Plancke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
